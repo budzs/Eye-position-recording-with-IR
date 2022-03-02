9c88b86cd0e968b1d8d594bff18b725e946e08ab
budzs <61503191+budzs@users.noreply.github.com>
2022-03-02 10:51:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import moviepy.editor as moviepy
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+# Configure IR stream
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.infrared, 640, 480, rs.format.y8, 30)
+
+# Start input streaming
+pipeline.start(config)
+
+
+# Ignore first 1sec for camera warm-up
+for i in range(30):
+    frames = pipeline.wait_for_frames()
+i = 0
+
+try:
+    while True:
+        # Read image
+        frames = pipeline.wait_for_frames()
+        infrared_frame = frames.first(rs.stream.infrared)
+        IR_image = np.asanyarray(infrared_frame.get_data())
+
+        # Display image
+        cv2.imshow('IR image', IR_image)
+
+        # Exit on ESC key
+        c = cv2.waitKey(1) % 0x100
+	
+	# Screenshot on SPACE key
+        if c % 256 == 32:
+            img = cv2.imshow('IR screenshot', IR_image)
+            filename = "D:\\PycharmProjects\\IR\\Rec 03.01\\" + str(i) + ".png"
+            cv2.imwrite(filename, IR_image)
+            i += 1
+        if c == 27:
+            break
+
+finally:
+    pipeline.stop()
+    cv2.destroyAllWindows()
+    pipeline.stop()
+    cv2.destroyAllWindows()
+
+
